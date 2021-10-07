683a35072110e0177ecf645a47abd1a37e64353f
Amber <70947915+abogerd1@users.noreply.github.com>
2021-10-07 11:42:57 +0200

    Car first setup

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,4 +1,14 @@
 package domain;
 
-public class Car {
-}
+import java.io.Serializable;
+
+public class Car implements Serializable {
+
+    public static final String ATTRIBUTE_ID = "id";
+    public static final String ATTRIBUTE_MODEL = "model";
+    public static final String ATTRIBUTE_CONSTRUCTION_YEAR = "construction_year";
+    public static final String ATTRIBUTE_TRANSMISSION = "transmission";
+    public static final String ATTRIBUTE_FUEL = "fuel";
+    public static final String ATTRIBUTE_PRICE_PER = "price_per"; /** price per: kilometer, liter fuel, day, hour, etc.**/
+    public static final String ATTRIBUTE_price = "price";
+    }
