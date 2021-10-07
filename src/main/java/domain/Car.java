bc2aa571e0f3bbf75fc3db2ca5246eb68db38691
Amber <70947915+abogerd1@users.noreply.github.com>
2021-10-07 12:47:17 +0200

    Car 2

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 
 public class Car implements Serializable {
 
-    public static final String ATTRIBUTE_ID = "id";
-    public static final String ATTRIBUTE_MODEL = "model";
-    public static final String ATTRIBUTE_CONSTRUCTION_YEAR = "construction_year";
-    public static final String ATTRIBUTE_TRANSMISSION = "transmission";
-    public static final String ATTRIBUTE_FUEL = "fuel";
-    public static final String ATTRIBUTE_PRICE_PER = "price_per"; /** price per: kilometer, liter fuel, day, hour, etc.**/
-    public static final String ATTRIBUTE_price = "price";
+    public static String id;
+    public static String constructionYear;
+    public static String transmission;
+    public static String fuel;
+    public static String pricePer; /** price per: kilometer, liter fuel, day, hour, etc.**/
+    public static int price;
+    private static String model;
+
 
     public  boolean petsAllowed;
     public boolean smokingAllowed;
