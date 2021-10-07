8a684df6ca03e1df3925c2c84025eeb023e54cdb
Amber <70947915+abogerd1@users.noreply.github.com>
2021-10-07 13:02:26 +0200

    Car 3

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,13 +5,12 @@ import java.io.Serializable;
 public class Car implements Serializable {
 
     public static String id;
+    public static String model;
     public static String constructionYear;
     public static String transmission;
     public static String fuel;
     public static String pricePer; /** price per: kilometer, liter fuel, day, hour, etc.**/
     public static int price;
-    private static String model;
-
 
     public  boolean petsAllowed;
     public boolean smokingAllowed;
@@ -45,9 +44,67 @@ public class Car implements Serializable {
         this.smokingAllowed = isSmokingAllowed();
         this.isofixPresent = isIsofixPresent();
     }
+    public String getId(){
+        return id;
+    }
 
+    public void setId(String id){
+        this.id = id;
+    }
 
+    public String getModel() {
+        return model;
+    }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getConstructionYear() {
+        return constructionYear;
+    }
+
+    public void setConstructionYear(String constructionYear) {
+        this.constructionYear = constructionYear;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public String getPricePer() {
+        return pricePer;
+    }
+
+    public void setPricePer(String pricePer) {
+        this.pricePer = pricePer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return "Car [id=" + id + ", model=" + model + ", construction year=" + constructionYear + ", transmission=" + transmission + ", fuel=" + fuel
+                +  " cost " + price + "per " + pricePer + "]";
+    }
 }
 
 
