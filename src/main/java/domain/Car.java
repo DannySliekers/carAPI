ad9d9f24ddf91c1d2ee87d48d6132775fe525596
Amber <70947915+abogerd1@users.noreply.github.com>
2021-10-07 12:33:24 +0200

    Car 1

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -11,4 +11,42 @@ public class Car implements Serializable {
     public static final String ATTRIBUTE_FUEL = "fuel";
     public static final String ATTRIBUTE_PRICE_PER = "price_per"; /** price per: kilometer, liter fuel, day, hour, etc.**/
     public static final String ATTRIBUTE_price = "price";
+
+    public  boolean petsAllowed;
+    public boolean smokingAllowed;
+    public boolean isofixPresent;
+
+    public boolean isPetsAllowed() {
+        return petsAllowed;
     }
+
+    public boolean isSmokingAllowed() {
+        return smokingAllowed;
+    }
+
+    public boolean isIsofixPresent() {
+        return isofixPresent;
+    }
+
+    public Car(){
+
+    }
+
+    public Car(String model, String constructionYear, String transmission, String fuel, String pricePer, int price) {
+        this.model = model;
+        this.constructionYear = constructionYear;
+        this.transmission = transmission;
+        this.fuel = fuel;
+        this.pricePer = pricePer;
+        this.price = price;
+
+        this.petsAllowed = isPetsAllowed();
+        this.smokingAllowed = isSmokingAllowed();
+        this.isofixPresent = isIsofixPresent();
+    }
+
+
+
+}
+
+
