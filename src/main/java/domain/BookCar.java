5523460667c8d6f68b5e238cee82daf368ca2c21
Amber <70947915+abogerd1@users.noreply.github.com>
2021-10-07 13:08:54 +0200

    Book a Car 1

---
+++ src/main/java/domain/BookCar.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class BookCar {
+
+    private Long reservationId;
+     private String carId;
+
+
+    public BookCar(){
+
+    }
+
+    public BookCar(Long reservationId, String carId){
+        this.reservationId = reservationId;
+        this.carId = carId;
+    }
+
+    public Long getReservationId(){
+        return reservationId;
+    }
+
+
+    public void setReservationId(Long reservationId){
+        this.reservationId = reservationId;
+    }
+
+    public String getCarId(){
+        return carId;
+    }
+
+    public void setCarId(String carId){
+        this.carId = carId;
+    }
+
+}
