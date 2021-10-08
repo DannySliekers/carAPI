a1e3891c332d807a48ed6e018fb3c2b87c11875f
DannySliekers <dannysliekers@hotmail.com>
2021-10-08 15:13:00 +0200

    Added mock carlisting

--- src/main/java/com/avanshogeschool/API/controller/CarListingController.java
+++ src/main/java/com/avanshogeschool/API/controller/CarListingController.java
@@ -1,15 +1,23 @@
 package com.avanshogeschool.API.controller;
 
+import com.avanshogeschool.API.repository.CarListingRepository;
+import com.avanshogeschool.API.domain.CarListing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/carlisting")
 public class CarListingController {
 
+    private final CarListingRepository carListingRepository;
+
+    public CarListingController(CarListingRepository carListingRepository) {
+        this.carListingRepository = carListingRepository;
+    }
+
     @GetMapping
-    public String getCarListing() {
-        return "test";
+    public Iterable<CarListing> getCarListing() {
+        return carListingRepository.findAll();
     }
 }
--- src/main/java/com/avanshogeschool/API/repository/DbInit.java
+++ src/main/java/com/avanshogeschool/API/repository/DbInit.java
@@ -1,4 +1,20 @@
 package com.avanshogeschool.API.repository;
 
+import com.avanshogeschool.API.domain.Car;
+import com.avanshogeschool.API.domain.CarListing;
+import com.avanshogeschool.API.domain.CarSize;
+import com.avanshogeschool.API.domain.CarType;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DbInit {
+    public DbInit(
+            CarListingRepository carListingRepository,
+            CarRepository carRepository
+    ) {
+        Car car = new Car("Mercedes", CarType.ELECTRIC, "automaat", CarSize.BIG);
+        carRepository.save(car);
+        CarListing carListing = new CarListing(car, 4000, true, "test", 800, 0.15);
+        carListingRepository.save(carListing);
+    }
 }
