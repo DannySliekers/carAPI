6315f8fddf16da1ac88daece7991e67e1e828f50
DannySliekers <dannysliekers@hotmail.com>
2021-10-14 16:23:10 +0200

    Can get and add user now

--- src/main/java/com/avanshogeschool/API/controller/UserController.java
+++ src/main/java/com/avanshogeschool/API/controller/UserController.java
@@ -2,10 +2,12 @@ package com.avanshogeschool.API.controller;
 
 import com.avanshogeschool.API.domain.User;
 import com.avanshogeschool.API.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -17,5 +19,14 @@ public class UserController {
         return userRepository.findAll();
     }
 
-
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User newUser) {
+        try {
+            User user = userRepository.save(newUser);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch(IllegalArgumentException e) {
+            System.out.println("Error during creation of" + newUser + e);
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
