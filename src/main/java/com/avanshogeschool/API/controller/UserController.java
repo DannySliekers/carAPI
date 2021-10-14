039e039325d65df956130b042de8c51820507845
DannySliekers <dannysliekers@hotmail.com>
2021-10-14 16:32:55 +0200

    Added update user

--- src/main/java/com/avanshogeschool/API/controller/UserController.java
+++ src/main/java/com/avanshogeschool/API/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.avanshogeschool.API.controller;
 
+import com.avanshogeschool.API.domain.Car;
 import com.avanshogeschool.API.domain.User;
 import com.avanshogeschool.API.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -37,5 +40,21 @@ public class UserController {
         }
         userRepository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id,
+                                         @RequestBody User newUser) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User oldUser = optionalUser.get();
+            oldUser.setName(newUser.getName());
+            oldUser.setUsername(newUser.getUsername());
+            oldUser.setHash(newUser.getHash());
+            return ResponseEntity.ok(userRepository.save(oldUser));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
